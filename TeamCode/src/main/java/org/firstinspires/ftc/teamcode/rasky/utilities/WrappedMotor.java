9f82ec2678ac95b9b4eed05f108cbd18f2bc2562
Iosif <iosif.andrei.stefan@gmail.com>
2022-10-14 16:45:06 +0300

    Solved a bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/utilities/WrappedMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/utilities/WrappedMotor.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * run them to a given position.
  *
  * @author Lucian
- * @version 1.0
+ * @version 1.1
  */
 public class WrappedMotor {
     public DcMotorEx motor;
@@ -61,7 +61,7 @@ public class WrappedMotor {
         else
             direction = -1;
 
-        if (Math.abs(Math.abs(targetPosition) - Math.abs(currentPosition)) > tolerance)
+        if (Math.abs(targetPosition - currentPosition) > tolerance)
             motor.setPower(speed * direction);
         else if (hold)
             motor.setPower(0.1);
